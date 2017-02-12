92b7fd69555a14ed537bd120dc30bc99bb45aab8
DanielUche <dank.uche@yahoo.com>
2017-02-12 01:04:04 +0100

    add-commit

---
+++ database/migrations/2017_02_11_235634_attendance.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Attendance extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('email');
+            $table->integer('status');
+            $table->string('staff_id');    
+            $table->date('a_date');
+            $table->string('a_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+}
